38f4b916d8d5a4704f32119099773a9cd2f9ef9d
Aleksey Halifostov <galifostov589@gmail.com>
2023-08-05 22:08:31 +0300

    corrected class Person

--- person.py
+++ person.py
@@ -5,9 +5,9 @@ class Person:
     def __init__(self, name, birth_date, gender, lastname="", surname="", death_date=""):
         self.name = f"{lastname.title()} {name.title()} {surname.title()}".strip()
         self.gender = gender.lower()
-        self.birth_date = birth_date.replace(" ", ".").replace("/", ".")
+        self.birth_date = birth_date.replace(" ", ".").replace("/", ".").replace("-", ".")
 
         if death_date:
-            self.death_date = death_date.replace(" ", ".").replace("/", ".")
+            self.death_date = death_date.replace(" ", ".").replace("/", ".").replace("-", ".")
         else:
             self.death_date = death_date
