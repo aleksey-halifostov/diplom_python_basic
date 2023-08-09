5a0c04eba913b7e630f61108616c9fd09fd200fa
Aleksey Halifostov <galifostov589@gmail.com>
2023-08-09 13:46:32 +0300

    corrected class Person

--- person.py
+++ person.py
@@ -4,6 +4,8 @@ class Person:
 
     def __init__(self, name, birth_date, gender, lastname="", surname="", death_date=""):
         self.name = f"{lastname.title()} {name.title()} {surname.title()}".strip()
+        if gender.lower() != "женщина":
+            gender = "мужчина"
         self.gender = gender.lower()
         self.birth_date = birth_date.replace(" ", ".").replace("/", ".").replace("-", ".")
 
